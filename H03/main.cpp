a57b69b88f85b1584e3dfed67825f53c7bebafa7
Sandesh Pathak <70244866+spathak0919@users.noreply.github.com>
2020-11-29 10:26:10 -0600

    Update main.cpp

--- H03/main.cpp
+++ H03/main.cpp
@@ -1,1 +1,15 @@
+void Hand::Sort()
+{
 
+  for(int i=0,i<Size(); i++)
+  {
+    if(Cards[i]->rank > Cards[j]->rank)
+    {
+      cout <<"Swap";
+      Card*temp =Cards[i]
+       Cards[i] = Cards[j];
+      Cards[j] = temp;
+      
+    }
+  }   
+}
